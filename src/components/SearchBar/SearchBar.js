c66ba59a71324e45aa1129da9c368887df4dcd41
dkonayuki <dkonayuki@gmail.com>
2018-03-14 11:20:24 +0900

    add SearchBar to Recipe page

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  FormControl,
+  Button
+} from 'react-bootstrap';
+
+import './SearchBar.css';
+
+import BeeImg from '../../assets/images/bee.svg';
+
+function SearchBar({
+  value,
+  onChange,
+  onSubmit
+}) {
+  function handleInputChange(e) {
+    onChange(e.target.value);
+  }
+
+  return (
+    <form className="search-bar">
+      <FormControl
+        type="text"
+        value={value}
+        onChange={handleInputChange}
+      />
+      <Button
+        className="search-bar__btn"
+        type="submit"
+        onClick={onSubmit}
+      >
+        <img src={BeeImg} alt="" />
+      </Button>
+    </form>
+  );
+}
+
+SearchBar.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func
+};
+
+SearchBar.defaultProps = {
+  value: '',
+  onChange: null,
+  onSubmit: null
+};
+
+export default SearchBar;
--- src/views/RecipePage/RecipePage.js
+++ src/views/RecipePage/RecipePage.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import Box from '../../components/Box';
 import { Helmet } from 'react-helmet';
+import Box from '../../components/Box';
+import SearchBar from '../../components/SearchBar';
 
-function RecipePage() {
-  return (
-    <div>
-      <Helmet>
-        <title>Recipe</title>
-      </Helmet>
-      <Box
-        withTitle
-        title="Recipe"
-        size="xlarge"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt quam id odio pellentesque, imperdiet molestie justo maximus. Praesent ut viverra leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus enim lectus, scelerisque id ornare non, vestibulum id lacus. Nulla faucibus sagittis elit vel blandit. Donec ac libero quis neque 
-      </Box>
-    </div>
-  );
+class RecipePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+
+    this.handleQueryChange = this.handleQueryChange.bind(this);
+  }
+
+  handleQueryChange(value) {
+    this.setState({
+      query: value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Recipe</title>
+        </Helmet>
+        <Box
+          size="xlarge"
+        >
+          <SearchBar
+            value={this.state.query}
+            onChange={this.handleQueryChange}
+          />
+        </Box>
+      </div>
+    );
+  }
 }
 
 export default RecipePage;
